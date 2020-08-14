1ff332fac49089f3f61bd11e40bd627e26729436
Estefan Hu <estefanhu074@gmail.com>
2020-08-13 22:11:09 -0700

    Updated styles

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ProgressBar from './ProgressBar.js';
+import React, { useState } from 'react';
+import ProgressBar from './ProgressBar';
 
 const UploadForm = () => {
-  const [file, setFile] = React.useState(null);
-  const [error, setError] = React.useState(null);
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
 
   const types = ['image/png', 'image/jpeg'];
 
-  const changeHandler = e => {
+  const handleChange = (e) => {
     let selected = e.target.files[0];
 
     if (selected && types.includes(selected.type)) {
@@ -15,24 +15,23 @@ const UploadForm = () => {
       setError('');
     } else {
       setFile(null);
-      setError('Please select an image file (png or jpeg)');
+      setError('Please select an image file (png or jpg)');
     }
-  }
+  };
 
   return (
     <form>
-      <input
-        className='input'
-        type='file'
-        onChange={changeHandler}
-      />
-      <div className='output'>
-        {error && <div className='error'>{error}</div>}
-        {file && <div>{file.name}</div>}
-        {file && <ProgressBar file={file} setFile={setFile} />}
+      <label>
+        <input type="file" onChange={handleChange} />
+        <span>+</span>
+      </label>
+      <div className="output">
+        { error && <div className="error">{ error }</div>}
+        { file && <div>{ file.name }</div> }
+        { file && <ProgressBar file={file} setFile={setFile} /> }
       </div>
     </form>
-  )
-};
+  );
+}
 
 export default UploadForm;
