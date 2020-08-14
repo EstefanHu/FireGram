9c4e7ef4fb697681948ec7a8f4cfb3f4c625c0b6
Estefan Hu <estefanhu074@gmail.com>
2020-08-13 21:17:12 -0700

    started firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyCv0fMmq7mIjmysk-mRvXhaatJwOre9sSU",
+  authDomain: "firegram-81a2a.firebaseapp.com",
+  databaseURL: "https://firegram-81a2a.firebaseio.com",
+  projectId: "firegram-81a2a",
+  storageBucket: "firegram-81a2a.appspot.com",
+  messagingSenderId: "852722621982",
+  appId: "1:852722621982:web:48034082560fec72bf980f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
