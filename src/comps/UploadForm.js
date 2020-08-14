623403a33299c1fc8b3d40c2f3154ad563566f01
Estefan Hu <estefanhu074@gmail.com>
2020-08-13 21:32:45 -0700

    added upload component

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
 const UploadForm = () => {
+  const [file, setFile] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  const types = ['image/png', 'image/jpeg'];
+
+  const changeHandler = e => {
+    let selected = e.target.files[0];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError('');
+    } else {
+      setFile(null);
+      setError('Please select an image file (png or jpeg)');
+    }
+  }
+
   return (
     <form>
-      <input type='file' />
+      <input
+        type='file'
+        onChange={changeHandler}
+      />
+      <div className='output'>
+        {error && <div className='error'>{error}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
     </form>
   )
 };
